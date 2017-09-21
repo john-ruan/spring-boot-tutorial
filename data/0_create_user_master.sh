ca421c7ae191fda300eb542ada282b1cc6747d25
sasaki-tomoya <sasaki.tomoya.0628@gmail.com>
2017-09-21 22:05:30 +0900

    Fix shell to create table

--- data/0_create_user_master.sh
+++ data/0_create_user_master.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 psql -U testuser -d testdb -c " \
+  DROP TABLE IF EXISTS user_master;\
   CREATE TABLE user_master ( \
     id SERIAL NOT NULL, \
     user_name VARCHAR(20) NOT NULL, \
+    password VARCHAR(20) NOT NULL, \
+    is_admin BOOLEAN DEFAULT FALSE NOT NULL, \
     PRIMARY KEY (id) \
   );
 "
